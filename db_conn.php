062eee3521c29d0e58dd09803f9724bd83b3410a
caneray <caneray34@hotmail.com>
2022-05-13 10:32:22 +0300

    Host işlemine göre düzenlendi

--- db_conn.php
+++ db_conn.php
@@ -1,9 +1,9 @@
 <?php
 
-$sname= "localhost";
-$unmae= "root";
-$password = "";
-$db_name = "webphp";
+$sname= "sql102.epizy.com";
+$unmae= "epiz_31714807";
+$password = "PrxD72Ewh0NZ";
+$db_name = "epiz_31714807_webphp";
 
 $conn = mysqli_connect($sname, $unmae, $password, $db_name);
 
